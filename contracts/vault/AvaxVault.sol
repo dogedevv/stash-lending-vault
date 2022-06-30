9e7ccbace01b35911fd73b38dd20bea61aa718d7
dogedevv <dodgedevv@github.com>
2022-06-30 15:42:17 +0700

    Lock pragma

--- contracts/vault/AvaxVault.sol
+++ contracts/vault/AvaxVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity ^0.8.10;
+pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
